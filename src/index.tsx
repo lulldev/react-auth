ee104ebc334b740f91bd5130d983aa2cc91d76bd
lulldev <lulldev@yandex.ru>
2018-09-23 12:32:29 +0300

    add redux support

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux'
+import configureStore from './store';
 import {Login, Profile} from './containers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render((
-  <BrowserRouter>
-    <Switch>
-      <Route exact={true} path="/" component={Login}/>
-      <Route path="/profile" component={Profile}/>
-    </Switch>
-  </BrowserRouter>
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path="/" component={Login}/>
+        <Route path="/profile" component={Profile}/>
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
 
 registerServiceWorker();
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/rootReducer';
+
+
+export default function configureStore(initialState = {}) {
+ return createStore(
+    rootReducer,
+    applyMiddleware(thunk),
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+ );
+}
